d4619cbf46929b9f4a5b7e2fcaa36989e1721712
Dongwankim1 <zkdlwu94@jiran.com>
2022-12-10 20:14:32 +0900

    * 리스트 아이템 TimeCheckItem 생성

--- component/Head.tsx
+++ component/Head.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, StatusBar,StyleSheet, View,Text} from "react-native";
+import {Button, StatusBar, StyleSheet, View, Text, Image, TouchableOpacity} from "react-native";
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 function Head(props) {
     const {top} = useSafeAreaInsets();
@@ -8,8 +8,12 @@ function Head(props) {
         <View style={[styles.statusBarPlaceholder,{height:top}]}></View>
         <View style={styles.block}>
             <StatusBar backgroundColor={"#3E444A"}/>
-            <Text>MapleOrder</Text>
-            <Button title={"추가"}></Button>
+            <Text style={styles.text}>MapleOrdersss</Text>
+            <TouchableOpacity activeOpacity={0.5}>
+                <View style={styles.buttonStyle}>
+                    <Image source={require('../assets/images/add_white.png')} style={styles.image}></Image>
+                </View>
+            </TouchableOpacity>
         </View>
         </>
     );
@@ -19,11 +23,30 @@ const styles = StyleSheet.create({
         backgroundColor:"#3E444A"
 
     },
+    buttonStyle:{
+      alignItems:'center',
+      justifyContent:'center',
+      width:48,
+      height:48,
+      backgroundColor:'#26a69a',
+      borderRadius:24
+    },
     block:{
+        height:64,
         flexDirection:"row",
         alignItems:"center",
+        justifyContent:'space-between',
         backgroundColor:"#3E444A",
-        padding:16,
+        borderTopWidth:1,
+        borderBottomWidth:1,
+        paddingHorizontal:16,
+    },
+    image:{
+        width:24,
+        height:24
+    },text:{
+        fontSize:16,
+        paddingVertical:8,
     }
 })
 
